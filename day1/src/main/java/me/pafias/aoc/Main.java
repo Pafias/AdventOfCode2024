d6ec1c1fdf7739aa1809390d9afab933b7620a3e
Pafias <29665404+Pafias@users.noreply.github.com>
2024-12-02 10:35:24 +0100

    optimization 💯

--- day1/src/main/java/me/pafias/aoc/Main.java
+++ day1/src/main/java/me/pafias/aoc/Main.java
@@ -8,19 +8,19 @@ import java.util.List;
 public class Main {
 
     public static int part1(List<String> lines) {
-        List<String> list1 = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
         lines.forEach(line -> {
             String[] split = line.split("   ");
-            list1.add(split[0].trim());
-            list2.add(split[1].trim());
+            list1.add(Integer.valueOf(split[0].trim()));
+            list2.add(Integer.valueOf(split[1].trim()));
         });
-        List<String> sortedList1 = list1.stream().sorted().toList();
-        List<String> sortedList2 = list2.stream().sorted().toList();
+        List<Integer> sortedList1 = list1.stream().sorted().toList();
+        List<Integer> sortedList2 = list2.stream().sorted().toList();
         int total = 0;
         for (int i = 0; i < sortedList1.size(); i++) {
-            int n1 = Integer.parseInt(sortedList1.get(i));
-            int n2 = Integer.parseInt(sortedList2.get(i));
+            int n1 = sortedList1.get(i);
+            int n2 = sortedList2.get(i);
             int distance = Math.abs(n2 - n1);
             total += distance;
         }
@@ -28,22 +28,22 @@ public class Main {
     }
 
     public static int part2(List<String> lines) {
-        List<String> list1 = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
         lines.forEach(line -> {
             String[] split = line.split("   ");
-            list1.add(split[0].trim());
-            list2.add(split[1].trim());
+            list1.add(Integer.valueOf(split[0].trim()));
+            list2.add(Integer.valueOf(split[1].trim()));
         });
-        List<String> sortedList1 = list1.stream().sorted().toList();
-        List<String> sortedList2 = list2.stream().sorted().toList();
+        List<Integer> sortedList1 = list1.stream().sorted().toList();
+        List<Integer> sortedList2 = list2.stream().sorted().toList();
         int similarityScore = 0;
-        for (String n1 : sortedList1) {
+        for (Integer n1 : sortedList1) {
             int occurrencesInList2 = 0;
             occurrencesInList2 = (int) sortedList2.stream()
                     .filter(n2 -> n2.equals(n1))
                     .count();
-            similarityScore += Integer.parseInt(n1) * occurrencesInList2;
+            similarityScore += n1 * occurrencesInList2;
         }
         return similarityScore;
     }
