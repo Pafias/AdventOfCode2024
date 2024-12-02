fc5a0b02a73a82881d600943e726596b6c065694
Pafias <29665404+Pafias@users.noreply.github.com>
2024-12-02 15:13:50 +0100

    Day 2 Part 2

--- day2/src/main/java/me/pafias/aoc/Main.java
+++ day2/src/main/java/me/pafias/aoc/Main.java
@@ -4,40 +4,71 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class Main {
 
     public static int part1(List<String> lines) {
         List<int[]> safe = new ArrayList<>();
-        List<int[]> unsafe = new ArrayList<>();
 
         for (String line : lines) {
             String[] split = line.split(" ");
             int[] ints = Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
-            int increasing = 0;
-            int decreasing = 0;
-            for (int i = 0; i < ints.length; i++) {
-                int current = ints[i];
-                if (i + 1 >= ints.length)
-                    break;
-                int next = ints[i + 1];
-                if (next > current && Math.abs(next - current) >= 1 && Math.abs(next - current) <= 3)
-                    increasing++;
-                else if (next < current && Math.abs(next - current) >= 1 && Math.abs(next - current) <= 3)
-                    decreasing++;
-            }
-            if (increasing == ints.length - 1 || decreasing == ints.length - 1)
+            if (isSafe(ints))
                 safe.add(ints);
-            else
-                unsafe.add(ints);
         }
 
         return safe.size();
     }
 
+    private static boolean isSafe(int[] ints) {
+        int increasing = 0;
+        int decreasing = 0;
+        for (int i = 0; i < ints.length; i++) {
+            int current = ints[i];
+            if (i + 1 >= ints.length)
+                break;
+            int next = ints[i + 1];
+            if (next > current && Math.abs(next - current) >= 1 && Math.abs(next - current) <= 3)
+                increasing++;
+            else if (next < current && Math.abs(next - current) >= 1 && Math.abs(next - current) <= 3)
+                decreasing++;
+        }
+        return increasing == ints.length - 1 || decreasing == ints.length - 1;
+    }
+
     public static int part2(List<String> lines) {
-        return -1;
+        Deque<int[]> safe = new ConcurrentLinkedDeque<>();
+        Deque<int[]> unsafe = new ConcurrentLinkedDeque<>();
+
+        for (String line : lines) {
+            String[] split = line.split(" ");
+            int[] ints = Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
+            if (isSafe(ints))
+                safe.add(ints);
+            else
+                unsafe.add(ints);
+        }
+
+        for (int[] values : unsafe) {
+            for (int i = 0; i < values.length; i++) {
+                List<Integer> copy = new ArrayList<>();
+                for (int value : values) {
+                    copy.add(value);
+                }
+                copy.remove(i);
+                int[] array = copy.stream().mapToInt(Integer::intValue).toArray();
+                if (isSafe(array)) {
+                    safe.add(array);
+                    unsafe.remove(values);
+                    break;
+                }
+            }
+        }
+
+        return safe.size();
     }
 
     public static List<String> readFileLines(String file) throws Exception {
